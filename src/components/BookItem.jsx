2948c4305654f43f15b970567953bfae946f0cce
timothydowd <timdowd1984@gmail.com>
2019-06-05 19:37:38 +0100

    pulls image url data into front end and renders

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,20 +1,65 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import PropTypes from 'prop-types'
 
 export class BookItem extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
+    state = {
+        imageUrl: '',
+        author: '',
+        isLoaded: false
+    }
+
+    componentDidMount() {
+
+        const getImageUrl = axios.get(`/wp-json/wp/v2/media/${this.props.book.featured_media}`).catch((error) => {
+            console.log('image error: ',error)
+        })
+        const getAuthor = axios.get(`/wp-json/wp/v2/users/${this.props.book.author}`)
+        
+        Promise.all([getImageUrl, getAuthor])
+        .then((resArray) => {
+            console.log('sdsddsd',resArray[0])
+            
+            if(resArray[0]){
+                this.setState({
+                    imageUrl: resArray[0].data.media_details.sizes.full.source_url,
+                    author: resArray[1].data.name,
+                    isLoaded: true
+                })
+            }
+            else {
+                this.setState({
+                    imageUrl: '',
+                    author: resArray[1].data.name,
+                    isLoaded: true
+                })
+            }
+            
+        })
+    }
+
     render() {
         const { title, excerpt } = this.props.book
-        return (
-            <div>
-                <h2>
-                    {title.rendered}
-                </h2>
-                <div dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
-                
-               
-                
-            </div>
-        )
+
+        if(this.state.isLoaded){
+            console.log('imageurls: ',this.state.imageUrl)
+            return (
+                <div>
+                    <h2 style={{marginBottom: '0'}}>
+                        {title.rendered}
+                    </h2>
+                    <small>Review by { this.state.author }</small>
+                    <img style={{width: '100%'}} src={this.state.imageUrl} alt={this.props.book.title.rendered}></img>
+                    <div dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
+                </div>
+            )
+        }
+
+        return null;
+        
     }
 }
 
